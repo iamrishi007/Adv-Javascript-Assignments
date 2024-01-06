9ef8098f7ecf09c945018dac1212300094581641
Rushikesh  Gaware <119421184+iamrishi007@users.noreply.github.com>
2024-01-07 00:17:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,60 +1,54 @@
-
 let arr = []
-class CustomArray {
-
-     constructor(...rest) {
-          this.size = rest.length;
-
-          for (let i = 0; i < rest.length; i++) {
-               arr.push(rest[i])
-          }
-
-     }
-
-     push(elments) {
-          arr[this.size++] = elments
-          console.log(arr);
-     }
-     pop() {
-          if (arr.length == 0) {
-               console.log("Stack is empty");
-          }
-          let delArray = arr[arr.length - 1]
-          arr.length = arr.length - 1
-          console.log(arr);
-     }
-
-     top() {
-          console.log(arr.length - 1);
-     }
-     print() {
-          console.log(arr);
-     }
-
-     printReverse() {
-          let output = "["
-          for (let i = arr.length - 1; i >= 0; i--) {
-
-               if (i < arr.length - 1) {
-                    output += ","
-               }
-               output += arr[i]
-          }
-
-          output += "]"
-          console.log(output);
-     }
-
-     arrSize() {
-          console.log(arr.length);
+function CustomArray(...rest) {
+     this.size = rest.length;
+     for (let i = 0; i < rest.length; i++) {
+          arr.push(rest[i]);
      }
 }
-let result = new CustomArray(1, 2, 3, 4, 5)
-// console.log(arr);
-
-result.push(55)
-result.pop()
-result.top()
-result.print()
-result.printReverse()
-result.arrSize()
+CustomArray.prototype = {
+     push: function (element) {
+          arr[this.size++] = element;
+          console.log(arr);
+     },
+     pop: function () {
+          if (arr.length === 0) {
+               console.log("Stack is empty");
+               return;
+          }
+          let deletedElement = arr.pop();
+          this.size = arr.length;
+          console.log(arr);
+          return deletedElement;
+     },
+     top: function () {
+          if (arr.length === 0) {
+               console.log("Stack is empty");
+               return;
+          }
+          console.log(arr[arr.length - 1]);
+     },
+     print: function () {
+          console.log(arr);
+     },
+     printReverse: function () {
+          let output = "[";
+          for (let i = arr.length - 1; i >= 0; i--) {
+               if (i < arr.length - 1) {
+                    output += ",";
+               }
+               output += arr[i];
+          }
+          output += "]";
+          console.log(output);
+     },
+     arrSize: function () {
+          console.log(arr.length);
+     },
+};
+let result = new CustomArray(1, 2, 3, 4, 5, 6);
+result.push(99);
+result.pop();
+result.top();
+result.print();
+result.printReverse();
+result.arrSize();
